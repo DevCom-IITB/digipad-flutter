ef95b6f36b2417adee89728fe726b010ba1f044f
Adithya Krishna <vineetha.kadithya2001@gmail.com>
2021-06-30 19:57:24 +0530

    Eraser, clear functionality implemented. Added a blank settings page.

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,14 @@ import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:digipad/settings.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: DigiCanvas(),
+    routes: {
+      '/': (context) => DigiCanvas(),
+      '/settings':(context) => Settings(),
+    },
   ));
 }
 class DigiCanvas extends StatefulWidget {
@@ -19,6 +23,7 @@ class DigiCanvas extends StatefulWidget {
 class _DigiCanvasState extends State<DigiCanvas> {
 
   final pts = <Offset>[];
+  bool isDrawing = true;
 
   @override
   Widget build(BuildContext context) {
@@ -29,14 +34,26 @@ class _DigiCanvasState extends State<DigiCanvas> {
             setState(() {
               final renderbox = context.findRenderObject() as RenderBox;
               final localposition = renderbox.globalToLocal(details.globalPosition);
-              pts.add(localposition);
+              if(isDrawing){
+                pts.add(localposition);
+              }else{
+                for(var i=0;i<pts.length;i++ ){
+                  if((pts[i]-localposition).distance<5) pts[i] = Offset.zero;
+                }
+              }
             });
           },
           onPanUpdate: (details) {
             setState(() {
               final renderbox = context.findRenderObject() as RenderBox;
               final localposition = renderbox.globalToLocal(details.globalPosition);
-              pts.add(localposition);
+              if(isDrawing){
+                pts.add(localposition);
+              }else{
+                for(var i=0;i<pts.length;i++ ){
+                  if((pts[i]-localposition).distance<5) pts[i] = Offset.zero;
+                }
+              }
             });
           },
           onPanEnd: (details) {
@@ -51,6 +68,39 @@ class _DigiCanvasState extends State<DigiCanvas> {
               width: MediaQuery.of(context).size.width,
             ),
           )
+      ), bottomNavigationBar: Padding(
+        padding: EdgeInsets.all(15.0),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(8)),
+            color: Colors.blue[500]
+          ),
+          padding: EdgeInsets.all(10.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              IconButton(onPressed: () {
+                setState(() {
+                  isDrawing = true;
+                });
+              }, icon: Icon(Icons.edit,size: 30,color: isDrawing?Colors.white:Colors.black,)),
+              IconButton(onPressed: () {
+                setState(() {
+                  isDrawing = false;
+                });
+              }, icon: Icon(Icons.auto_fix_high,size: 30,color: isDrawing?Colors.black:Colors.white,)),
+              IconButton(onPressed: () {
+                setState(() {
+                  pts.clear();
+                });
+              }, icon: Icon(Icons.clear,size: 30,)),
+              IconButton(onPressed: () {
+                Navigator.pushNamed(context, '/settings');
+              }, icon: Icon(Icons.settings,size: 30,)),
+            ],
+          ),
+        ),
       ),
     );;
   }
---
+++ lib/settings.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Settings extends StatelessWidget {
+  const Settings({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Settings"),
+      ),
+      body:Center(
+        child: Text("Work in Progress"),
+
+      ) ,
+    );;
+  }
+}

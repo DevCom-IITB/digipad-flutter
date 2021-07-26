16f90de57343c115486c00ff9cc7f8e4936020d9
Ved Khandekar <vedk90@gmail.com>
2021-07-26 23:16:46 +0530

    Formatted code with dartfmt.

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,11 @@ void main() {
   runApp(MaterialApp(
     routes: {
       '/': (context) => DigiCanvas(),
-      '/settings':(context) => Settings(),
+      '/settings': (context) => Settings(),
     },
   ));
 }
+
 class DigiCanvas extends StatefulWidget {
   const DigiCanvas({Key? key}) : super(key: key);
 
@@ -24,11 +25,11 @@ class DigiCanvas extends StatefulWidget {
 }
 
 class _DigiCanvasState extends State<DigiCanvas> {
-
   final pts = <Offset>[];
   bool isDrawing = true;
   bool isConnected = false;
-  String ipAddress = '10.0.2.2'; //default value for local host while using emulator
+  String ipAddress =
+      '192.168.1.6'; //default value for local host while using emulator
   var socket;
   late StreamSubscription socketListener;
 
@@ -39,7 +40,6 @@ class _DigiCanvasState extends State<DigiCanvas> {
 
     // listen for responses from the server
     socketListener = socket.listen(
-
       // handle data from the server
       (Uint8List data) {
         final serverResponse = String.fromCharCodes(data);
@@ -71,25 +71,26 @@ class _DigiCanvasState extends State<DigiCanvas> {
   }
 
   Future<dynamic> createAlertDialog(BuildContext context) {
-
     TextEditingController controller = new TextEditingController();
 
-    return showDialog(context: context, builder: (context){
-      return AlertDialog(
-        title: Text("IP Address"),
-        content: TextField(
-          controller: controller,
-        ) ,
-        actions: <Widget>[
-          ElevatedButton(
-            onPressed: (){
-              Navigator.of(context).pop(controller.text.toString());
-            },
-            child: Text("Connect"),
-          )
-        ],
-      );
-    });
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("IP Address"),
+            content: TextField(
+              controller: controller,
+            ),
+            actions: <Widget>[
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pop(controller.text.toString());
+                },
+                child: Text("Connect"),
+              )
+            ],
+          );
+        });
   }
 
   @override
@@ -100,12 +101,14 @@ class _DigiCanvasState extends State<DigiCanvas> {
           onPanStart: (details) {
             setState(() {
               final renderbox = context.findRenderObject() as RenderBox;
-              final localposition = renderbox.globalToLocal(details.globalPosition);
-              if(isDrawing){
+              final localposition =
+                  renderbox.globalToLocal(details.globalPosition);
+              if (isDrawing) {
                 pts.add(localposition);
-              }else{
-                for(var i=0;i<pts.length;i++ ){
-                  if((pts[i]-localposition).distance<5) pts[i] = Offset.zero;
+              } else {
+                for (var i = 0; i < pts.length; i++) {
+                  if ((pts[i] - localposition).distance < 5)
+                    pts[i] = Offset.zero;
                 }
               }
             });
@@ -113,13 +116,15 @@ class _DigiCanvasState extends State<DigiCanvas> {
           onPanUpdate: (details) {
             setState(() {
               final renderbox = context.findRenderObject() as RenderBox;
-              final localposition = renderbox.globalToLocal(details.globalPosition);
-              if(isDrawing){
+              final localposition =
+                  renderbox.globalToLocal(details.globalPosition);
+              if (isDrawing) {
                 pts.add(localposition);
                 sendMessage(localposition.toString());
-              }else{
-                for(var i=0;i<pts.length;i++ ){
-                  if((pts[i]-localposition).distance<5) pts[i] = Offset.zero;
+              } else {
+                for (var i = 0; i < pts.length; i++) {
+                  if ((pts[i] - localposition).distance < 5)
+                    pts[i] = Offset.zero;
                 }
               }
             });
@@ -135,86 +140,109 @@ class _DigiCanvasState extends State<DigiCanvas> {
               height: MediaQuery.of(context).size.height,
               width: MediaQuery.of(context).size.width,
             ),
-          )
-      ), bottomNavigationBar: Padding(
+          )),
+      bottomNavigationBar: Padding(
         padding: EdgeInsets.all(15.0),
         child: Container(
           decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(8)),
-            color: Colors.blue[500]
-          ),
+              borderRadius: BorderRadius.all(Radius.circular(8)),
+              color: Colors.blue[500]),
           padding: EdgeInsets.all(10.0),
           child: Row(
             crossAxisAlignment: CrossAxisAlignment.end,
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-              IconButton(onPressed: () {
-                setState(() {
-                  isDrawing = true;
-                  sendMessage("Pencil"); // send message to the server that pencil is selected
-                });
-              }, icon: Icon(Icons.edit,size: 30,color: isDrawing?Colors.white:Colors.black,)),
-              IconButton(onPressed: () {
-                setState(() {
-                  isDrawing = false;
-                  sendMessage("Eraser"); //send message to server that eraser is selected
-                });
-              }, icon: Icon(Icons.auto_fix_high,size: 30,color: isDrawing?Colors.black:Colors.white,)),
-              IconButton(onPressed: () {
-                setState(() {
-                  pts.clear();
-                  socketListener.cancel(); //terminate listening to server
-                  socket.destroy(); //terminate socket connection
-                  isConnected = false;
-                });
-              }, icon: Icon(Icons.clear,size: 30,)),
-              IconButton(onPressed: () {
-                Navigator.pushNamed(context, '/settings');
-              }, icon: Icon(Icons.settings,size: 30,)),
-              IconButton(onPressed: () {
-                createAlertDialog(context).then((value)  {
-                  setState(() {
-                    ipAddress = value;
-                    comm();
-                    isConnected = true;
-                  });
-                } );
-
-              }, icon: Icon(Icons.bluetooth,size: 30,color: isConnected?Colors.white:Colors.black,)),
+              IconButton(
+                  onPressed: () {
+                    setState(() {
+                      isDrawing = true;
+                      sendMessage(
+                          "Pencil"); // send message to the server that pencil is selected
+                    });
+                  },
+                  icon: Icon(
+                    Icons.edit,
+                    size: 30,
+                    color: isDrawing ? Colors.white : Colors.black,
+                  )),
+              IconButton(
+                  onPressed: () {
+                    setState(() {
+                      isDrawing = false;
+                      sendMessage(
+                          "Eraser"); //send message to server that eraser is selected
+                    });
+                  },
+                  icon: Icon(
+                    Icons.auto_fix_high,
+                    size: 30,
+                    color: isDrawing ? Colors.black : Colors.white,
+                  )),
+              IconButton(
+                  onPressed: () {
+                    setState(() {
+                      pts.clear();
+                      socketListener.cancel(); //terminate listening to server
+                      socket.destroy(); //terminate socket connection
+                      isConnected = false;
+                    });
+                  },
+                  icon: Icon(
+                    Icons.clear,
+                    size: 30,
+                  )),
+              IconButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/settings');
+                  },
+                  icon: Icon(
+                    Icons.settings,
+                    size: 30,
+                  )),
+              IconButton(
+                  onPressed: () {
+                    createAlertDialog(context).then((value) {
+                      setState(() {
+                        ipAddress = value;
+                        comm();
+                        isConnected = true;
+                      });
+                    });
+                  },
+                  icon: Icon(
+                    Icons.bluetooth,
+                    size: 30,
+                    color: isConnected ? Colors.white : Colors.black,
+                  )),
             ],
           ),
         ),
       ),
-    );;
+    );
   }
 }
 
-
-class DigiPainter extends CustomPainter{
-
+class DigiPainter extends CustomPainter {
   final pts;
-  DigiPainter(this.pts):super();
+  DigiPainter(this.pts) : super();
 
   @override
   void paint(Canvas canvas, Size size) {
     final paint = Paint()
-    ..color = Colors.black
-    ..strokeWidth = 3.0
-    ..isAntiAlias = true;
-    for(var i =0; i<pts.length-1;i++){
-      if(pts[i]!=Offset.zero&& pts[i+1]!=Offset.zero){
-        canvas.drawLine(pts[i], pts[i+1], paint);
-      }
-      else if(pts[i]!=Offset.zero&&pts[i+1]==Offset.zero){
+      ..color = Colors.black
+      ..strokeWidth = 3.0
+      ..isAntiAlias = true;
+    for (var i = 0; i < pts.length - 1; i++) {
+      if (pts[i] != Offset.zero && pts[i + 1] != Offset.zero) {
+        canvas.drawLine(pts[i], pts[i + 1], paint);
+      } else if (pts[i] != Offset.zero && pts[i + 1] == Offset.zero) {
         canvas.drawPoints(PointMode.points, [pts[i]], paint);
       }
     }
-
   }
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
     return true;
   }
-
 }
